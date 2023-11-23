146ce802f4175f2d928a98f492dd2417a935cb2f
alexm <alex.manea12@gmail.com>
2023-11-23 02:28:18 +0200

    Add actors detail page and update database

--- theater/urls.py
+++ theater/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("actori", views.get_actors, name="actors-page"),
-    path("actori/<slug:slug>", views.actor_detail,
-         name="post-detail-page")
+    path("actori/<str:name>", views.actor_detail,
+         name="actori-detail-page")
 ]
--- theater/views.py
+++ theater/views.py
@@ -5,28 +5,40 @@ from django.db import connection
 
 def get_actors(request):
     with connection.cursor() as cursor:
-        cursor.execute("""SELECT A.Nume, A.Prenume, AR.Rol
-                          FROM Angajati A
-                          INNER JOIN AngajatiActori AA
-                          ON A.AngajatID = AA.AngajatID
-                          INNER JOIN ActoriReprezentatii AR
-                          ON AA.AngajatiActoriID = AR.AngajatiActoriID;""")
+        cursor.execute("""SELECT 
+                            A.Nume, 
+                            A.Prenume, 
+                            STUFF((
+                                SELECT ', ' + AR.Rol
+                                FROM AngajatiActori AA
+                                INNER JOIN ActoriReprezentatii AR ON AA.AngajatiActoriID = AR.AngajatiActoriID
+                                WHERE A.AngajatID = AA.AngajatID
+                                FOR XML PATH(''), TYPE
+                            ).value('.', 'NVARCHAR(MAX)'), 1, 2, '') AS CombinedRoles
+                          FROM Angajati A; """)
         actori = cursor.fetchall()
+        actori_dicts = [{'Nume': actor[0], 'Prenume': actor[1], 'Rol': actor[2]} for actor in actori]
+        photos = {
+           "Ionescu": "ionescu-maria.jpg", 
+           "Florescu": "florescu-adrian.jpg", 
+           "Radulescu": "radulescu-daniel.jpg"
+            }
         return render(request, "theater/actori.html", {
-            "actori": actori
+            "actori": actori_dicts,
+            "photos": photos
         })
     
-def actor_detail(request, slug):
+def actor_detail(request, name):
     with connection.cursor() as cursor:
         cursor.execute("""SELECT A.Nume,
                                  A.Prenume
                            FROM Angajati A
                            INNER JOIN AngajatiActori AA
                            ON A.AngajatID = AA.AngajatID
-                           INNER JOIN ActoriReprezentanti AR
-                           ON A.AngajatID = AR.AngajatID""")
+                       """)
         actori = cursor.fetchall()
-        print(actori)
-        return render(request, "theater/actori.html", {
-            "actori": actori
+        actori_dicts = [{'Nume': actor[0], 'Prenume': actor[1]} for actor in actori]
+        identified_actor = next(actor for actor in actori_dicts if actor['Nume'] == name)
+        return render(request, "theater/actor_details.html", {
+            "actor": identified_actor
         })
